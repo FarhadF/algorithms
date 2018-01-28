1660ea694dba4b18d745ab04a085942a668d1bb4
FarhadF <farhad.farahi@gmail.com>
2018-01-28 09:28:37 +0330

    fixed function names

--- binarysearch/binarysearch_recursive.go
+++ binarysearch/binarysearch_recursive.go
@@ -5,11 +5,11 @@ import "fmt"
 func main() {
 	slice := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
 	target := 9
-	result := binarySearch(slice, target)
+	result := binarySearchRecursive(slice, target)
 	fmt.Println("result:", result)
 }
 
-func binarySearch(slice []int, target int) int {
+func binarySearchRecursive(slice []int, target int) int {
 	l := len(slice)
 	//fmt.Println("slice:", slice)
 	//fmt.Println("length", l)
@@ -23,9 +23,9 @@ func binarySearch(slice []int, target int) int {
 		if slice[middle] == target {
 			return target
 		} else if slice[middle] <= target {
-			return binarySearch(slice[middle+1:], target)
+			return binarySearchRecursive(slice[middle+1:], target)
 		} else {
-			return binarySearch(slice[:middle], target)
+			return binarySearchRecursive(slice[:middle], target)
 		}
 	}
 }
--- binarysearch/binarysearch_recursive_test.go
+++ binarysearch/binarysearch_recursive_test.go
@@ -4,15 +4,15 @@ import "testing"
 
 func TestBinarySearch(t *testing.T) {
 	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
-	result := binarySearch(slice, 8)
+	result := binarySearchRecursive(slice, 8)
 	if result != 8 {
 		t.Error("expecting 8 but got: ", result)
 	}
-	result = binarySearch(slice, 2)
+	result = binarySearchRecursive(slice, 2)
 	if result != 2 {
 		t.Error("expecting 2 but got: ", result)
 	}
-	result = binarySearch(slice, 212)
+	result = binarySearchRecursive(slice, 212)
 	if result != -1 {
 		t.Error("expecting -1 but got: ", result)
 	}
