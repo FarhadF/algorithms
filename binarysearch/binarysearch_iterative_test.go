6b6cacd7aefc817245a4a092f6d4df426859b9d2
FarhadF <farhad.farahi@gmail.com>
2018-01-28 09:37:14 +0330

    added testing for binarysearch

---
+++ binarysearch/binarysearch_iterative_test.go
@@ -0,0 +1,20 @@
+package main
+
+import "testing"
+
+func TestBinarySearchIterative(t *testing.T) {
+	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	result := binarySearchIterative(slice, 8)
+	if result != 8 {
+		t.Error("expecting 8 but got: ", result)
+	}
+	result = binarySearchIterative(slice, 2)
+	if result != 2 {
+		t.Error("expecting 2 but got: ", result)
+	}
+	result = binarySearchIterative(slice, 212)
+	if result != -1 {
+		t.Error("expecting -1 but got: ", result)
+	}
+}
+
