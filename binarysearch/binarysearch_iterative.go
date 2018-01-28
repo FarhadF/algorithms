5d925abada9022514cc5ce8e259f6bacb1542c86
FarhadF <farhad.farahi@gmail.com>
2018-01-28 09:27:12 +0330

    separate function for binarysearch

--- binarysearch/binarysearch_iterative.go
+++ binarysearch/binarysearch_iterative.go
@@ -4,31 +4,36 @@ import "fmt"
 
 func main() {
 	slice := []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	target := 1
+	target := 3
+	fmt.Println(binarySearchIterative(slice, target))
+}
+
+
+func binarySearchIterative(slice []int, target int) int {
 	low := 0
 	high := len(slice)
 	for {
 		fmt.Println("slice:", slice)
 		fmt.Println("high:", high)
 		fmt.Println("low", low)
-		if low >= high {
-			fmt.Println("result: -1")
-			break
-		}
 		middle := (low + high) / 2
+		if low > high || middle >= len(slice) {
+			fmt.Println("result: -1")
+			return -1
+		}
+
 		fmt.Println("middle:", middle)
 		if slice[middle] == target {
 			fmt.Println("result:", middle)
-			break
+			return middle
 		}
-		if slice[middle] >= target {
+		if slice[middle] > target {
 			high = middle - 1
 			continue
 		}
-		if slice[middle] <= target {
+		if slice[middle] < target {
 			low = middle + 1
 			continue
 		}
-
 	}
 }
